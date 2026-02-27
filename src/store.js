2fbbca215a9dbf27d8ef56ff639dc14e0ee8d266
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-02-27 03:06:06 +0000

    added more noted to the store.js

--- src/store.js
+++ src/store.js
@@ -14,22 +14,22 @@ export const initialStore=()=>{
   }
 }
 
-export default function storeReducer(store, action = {}) {
-  switch(action.type){
+export default function storeReducer(store, action = {}) { // parameters pass store and action
+  switch(action.type){ // this block determines how to update the state based on the "intent"
     case 'add_task':
 
-      const { id,  color, } = action.payload
+      const { id,  color, } = action.payload // its extracts id and color form th action.payload
 
       return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        ...store, // reducers never modify the existing state directly. Instead, they use the spread operator(...store) to create a shallow copy of the beofre applying updtates.
+        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo)) // 
       };
-    case "update_contacts":
+    case "update_contacts": // this completly replaces the contacts array in the store with whatever is provided in the action.payload
       return {
         ...store,
         contacts: action.payload
       }
     default:
-      throw Error('Unknown action.');
+      throw Error('Unknown action.'); // if a unknown action type is sent, it throws an error. This is a common patter in React's useReducer to catch bugs early.
   }    
 }
